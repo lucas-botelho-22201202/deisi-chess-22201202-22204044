03c09d34844d78572986f943ee6f5076d232bbfc
DESKTOP-2MTF3A5\lbotelho <l_botelho@hotmail.com>
2023-10-30 17:45:56 +0000

    Change logic to hide negative coordenates

--- src/pt/ulusofona/lp2/deisichess/Piece.java
+++ src/pt/ulusofona/lp2/deisichess/Piece.java
@@ -85,8 +85,8 @@ public class Piece {
         properties[2] = String.valueOf(team);
         properties[3] = getNickName();
         properties[4] = getStatus();
-        properties[5] = getX() == -1 ? "" : String.valueOf(getX());
-        properties[6] = getY() == -1 ? "" : String.valueOf(getY());
+        properties[5] = status.equals(Piece.PIECE_IS_CAPTURED) ? "" : String.valueOf(getX());
+        properties[6] = status.equals(Piece.PIECE_IS_CAPTURED) ? "" : String.valueOf(getY());
 
         return properties;
     }
