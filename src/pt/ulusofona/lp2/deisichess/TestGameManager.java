2067809c06f61f5ff3014bfcdfe3def5402256cf
DESKTOP-2MTF3A5\lbotelho <l_botelho@hotmail.com>
2023-10-30 17:16:16 +0000

    unit tests

--- src/pt/ulusofona/lp2/deisichess/TestGameManager.java
+++ src/pt/ulusofona/lp2/deisichess/TestGameManager.java
@@ -19,8 +19,42 @@ public class TestGameManager {
                 {1, 2, 1, 1},
                 {3, 0, 2, 0},
                 {1, 1, 2, 1},
-                {1, 0, 1, 1},
+                {2, 0, 1, 1},
                 {2, 1, 1, 1}
+        };
+
+        for (int[] move : moves) {
+            int x0 = move[0];
+            int y0 = move[1];
+            int x1 = move[2];
+            int y1 = move[3];
+
+            gameManager.move(x0, y0, x1, y1);
+            if (gameManager.gameOver()){
+                break;
+            }
+        }
+
+        var gameResult = gameManager.getGameResults();
+
+        String expectedText = "JOGO DE CRAZY CHESS,Resultado: VENCERAM AS BRANCAS,---,Equipa das Pretas,0,3,0,Equipa das Brancas,3,3,0";
+
+        ArrayList<String> listOfExpectedTexts = new ArrayList<>(Arrays.asList(expectedText.split(",")));
+
+        for (int i = 0; i < listOfExpectedTexts.size(); i++) {
+            Assertions.assertEquals(listOfExpectedTexts.get(i), gameResult.get(i) , "test_White_Victory didn't match at index:" + i);
+        }
+    }
+
+    @Test
+    public void test_White_Victory_1Move(){
+        var gameManager = new GameManager();
+
+        var gameLoaded = gameManager.loadGame(new File("test-files/4x4_1move_victory_white.txt"));
+
+        int[][] moves = {
+                {0, 0, 1, 1},
+                {0, 1, 1, 1}
         };
 
         for (int[] move : moves) {
@@ -38,12 +72,12 @@ public class TestGameManager {
 
         var gameResult = gameManager.getGameResults();
 
-        String expectedText = "JOGO DE CRAZY CHESS, Resultado: VENCERAM AS BRANCAS, ---, Equipa das Pretas, 0, 2, 0, Equipa das Brancas, 2, 2, 0";
+        String expectedText = "JOGO DE CRAZY CHESS,Resultado: VENCERAM AS BRANCAS,---,Equipa das Pretas,0,1,0,Equipa das Brancas,1,1,0";
 
         ArrayList<String> listOfExpectedTexts = new ArrayList<>(Arrays.asList(expectedText.split(",")));
 
         for (int i = 0; i < listOfExpectedTexts.size(); i++) {
-            Assertions.assertEquals(listOfExpectedTexts.get(i), gameResult.get(i) , "test_White_Victory result matched at index:" + i);
+            Assertions.assertEquals(listOfExpectedTexts.get(i), gameResult.get(i) , "test_White_Victory_1Move didn't match at index:" + i);
         }
 
 
