ec5399d9286d644d3064802d83d0432085870468
DESKTOP-2MTF3A5\lbotelho <l_botelho@hotmail.com>
2023-12-09 15:59:56 +0000

    rename

--- src/pt/ulusofona/lp2/deisichess/StatisticsKt.kt
+++ src/pt/ulusofona/lp2/deisichess/StatisticsKt.kt
@@ -1,6 +1,6 @@
 package pt.ulusofona.lp2.deisichess
 
-public class Statistics {
+public class StatisticsKt {
     fun getStatsCalculator(): StatType{
         return StatType.PECAS_MAIS_CAPTURADAS;
     }
