abdf9c6efc7ec033891daf235fb3a98f0015b18f
DESKTOP-2MTF3A5\lbotelho <l_botelho@hotmail.com>
2023-12-09 17:27:50 +0000

    compilation errors dp

--- src/pt/ulusofona/lp2/deisichess/StatisticsKt.kt
+++ src/pt/ulusofona/lp2/deisichess/StatisticsKt.kt
@@ -1,12 +1,4 @@
 package pt.ulusofona.lp2.deisichess
-
-public class StatisticsKt {
-    fun getStatsCalculator(type: StatType): () -> ArrayList<String> {
-        return {
-            // Your logic to calculate stats and return ArrayList<String> goes here
-            val statsResult = arrayListOf("Stat1", "Stat2", "Stat3")
-            statsResult
-        }
-    }
-
+fun getStatsCalculator(type: StatType): (GameManager) -> ArrayList<String> {
+    return { gameManager -> ArrayList() }
 }
