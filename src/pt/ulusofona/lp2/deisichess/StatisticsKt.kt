bc67117d4623a5763231138960fc931f7413e7b9
DESKTOP-2MTF3A5\lbotelho <l_botelho@hotmail.com>
2023-12-09 16:10:10 +0000

    compilation errors dp

--- src/pt/ulusofona/lp2/deisichess/StatisticsKt.kt
+++ src/pt/ulusofona/lp2/deisichess/StatisticsKt.kt
@@ -1,7 +1,12 @@
 package pt.ulusofona.lp2.deisichess
 
 public class StatisticsKt {
-    fun getStatsCalculator(): StatType{
-        return StatType.PECAS_MAIS_CAPTURADAS;
+    fun getStatsCalculator(type: StatType): () -> ArrayList<String> {
+        return {
+            // Your logic to calculate stats and return ArrayList<String> goes here
+            val statsResult = arrayListOf("Stat1", "Stat2", "Stat3")
+            statsResult
+        }
     }
+
 }
