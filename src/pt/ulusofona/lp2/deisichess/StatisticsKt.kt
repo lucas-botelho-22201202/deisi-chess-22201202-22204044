0a21256aa599f7660236d198de2ad682e8552ad2
Lucas Botelho <Lucas.Botelho@merkle.com>
2023-12-13 19:31:35 +0000

    remove StatisticsKt.kt companion object

--- src/pt/ulusofona/lp2/deisichess/StatisticsKt.kt
+++ src/pt/ulusofona/lp2/deisichess/StatisticsKt.kt
@@ -2,59 +2,58 @@ package pt.ulusofona.lp2.deisichess
 
 import pt.ulusofona.lp2.deisichess.pieces.Piece
 
-class StatisticsKt {
-    companion object {
-        @JvmStatic
-        fun getStatsCalculator(type: StatType): (GameManager) -> ArrayList<String> {
-            return when (type) {
-                StatType.TOP_5_CAPTURAS -> ::topCincoCapturas
-                StatType.PECAS_MAIS_BARALHADAS -> ::pecaMaisBaralhada
-                StatType.TOP_5_PONTOS -> ::topCincoPontos
-                StatType.PECAS_MAIS_5_CAPTURAS -> ::pecasMaisCincoCapturas
-                StatType.TIPOS_CAPTURADOS -> ::tiposCapturados
-            }
+fun getStatsCalculator(type: StatType): (GameManager) -> ArrayList<String> {
+    return when (type) {
+        StatType.TOP_5_CAPTURAS -> ::topCincoCapturas
+        StatType.PECAS_MAIS_BARALHADAS -> ::pecaMaisBaralhada
+        StatType.TOP_5_PONTOS -> ::topCincoPontos
+        StatType.PECAS_MAIS_5_CAPTURAS -> ::pecasMaisCincoCapturas
+        StatType.TIPOS_CAPTURADOS -> ::tiposCapturados
+    }
 
-        }
+}
 
-        fun topCincoCapturas(gameManager: GameManager): ArrayList<String> {
-            return ArrayList(gameManager.board.pieces()
-                    .sortedByDescending { it.numCapturas }
-                    .take(5)
-                    .map { "${it.nickName} (${teamNameToString(it.team)}) fez ${it.numCapturas} capturas" })
-        }
+fun topCincoCapturas(gameManager: GameManager): ArrayList<String> {
+    return ArrayList(gameManager.board.pieces()
+            .sortedByDescending { it.numCapturas }
+            .take(5)
+            .map { "${it.nickName} (${teamNameToString(it.team)}) fez ${it.numCapturas} capturas" })
+}
 
-        fun pecaMaisBaralhada(gameManager: GameManager): ArrayList<String> {
-            return ArrayList(gameManager.board.pieces()
-                    .filter { it.numInvalidMoves > 0 }
-                    .sortedByDescending { it.numInvalidMoves.toDouble() / (it.numInvalidMoves + it.numValidMoves)  }
-                    .take(3)
-                    .map { "${it.team}:${it.nickName}:${it.numInvalidMoves}:${it.numValidMoves}" })
-        }
+fun pecaMaisBaralhada(gameManager: GameManager): ArrayList<String> {
+    return ArrayList(gameManager.board.pieces()
+            .filter { it.numInvalidMoves > 0 }
+            .sortedByDescending { it.numInvalidMoves.toDouble() / (it.numInvalidMoves + it.numValidMoves) }
+            .take(3)
+            .map { "${it.team}:${it.nickName}:${it.numInvalidMoves}:${it.numValidMoves}" })
+}
 
-        fun topCincoPontos(gameManager: GameManager): ArrayList<String> {
-            return ArrayList(gameManager.board.pieces()
-                    .filter { it.capturedPoints > 0 }
-                    .sortedWith(compareByDescending<Piece> { it.capturedPoints }.thenBy { it.nickName })
-                    .take(5)
-                    .map { "${it.nickName} (${teamNameToString(it.team)}) tem ${it.capturedPoints} pontos" })
-        }
+fun topCincoPontos(gameManager: GameManager): ArrayList<String> {
+    return ArrayList(gameManager.board.pieces()
+            .filter { it.capturedPoints > 0 }
+            .sortedWith(compareByDescending<Piece> { it.capturedPoints }.thenBy { it.nickName })
+            .take(5)
+            .map { "${it.nickName} (${teamNameToString(it.team)}) tem ${it.capturedPoints} pontos" })
+}
 
-        fun pecasMaisCincoCapturas(gameManager: GameManager): ArrayList<String> {
-            return ArrayList(gameManager.board.pieces()
-                    .filter { it.numCapturas > 5 }
-                    .map { "${teamNameToString(it.team)}:${it.nickName}:${it.numCapturas}" })
-        }
+fun pecasMaisCincoCapturas(gameManager: GameManager): ArrayList<String> {
+    return ArrayList(gameManager.board.pieces()
+            .filter { it.numCapturas > 5 }
+            .map { "${teamNameToString(it.team)}:${it.nickName}:${it.numCapturas}" })
+}
 
-        fun tiposCapturados(gameManager: GameManager): ArrayList<String> {
-            return ArrayList(gameManager.nameOfPiecesCaptured.distinctBy { it }.sorted())
-        }
+fun tiposCapturados(gameManager: GameManager): ArrayList<String> {
+    return ArrayList(gameManager.board.pieces()
+            .distinctBy { it.type }
+            .map { it.toString() }
+            .sorted())
+}
 
-        private fun teamNameToString(teamId: Int): String {
-            return if (teamId == Piece.BLACK_TEAM) {
-                "PRETA"
-            } else {
-                "BRANCA"
-            }
-        }
+private fun teamNameToString(teamId: Int): String {
+    return if (teamId == Piece.BLACK_TEAM) {
+        "PRETA"
+    } else {
+        "BRANCA"
     }
 }
+
